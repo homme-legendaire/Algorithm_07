74c2e6912d6ffded6f295a62d2f8127bcf575315
homme-legendaire <53377700+homme-legendaire@users.noreply.github.com>
2021-07-20 16:45:08 +0900

    직각삼각형
    4153

--- 23k.c
+++ 23k.c
@@ -1,16 +1,25 @@
 #pragma warning (disable:4996)
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 int main() {
-	int x, y, w, h, a = 0, b = 0;
-	scanf("%d %d %d %d", &x, &y, &w, &h);
-	a = w - x;
-	b = h - y;
-	if (a > x) a = x;
-	if (b > y) b = y;
-
-	if (a > b) printf("%d", b);
-	else printf("%d", a);
+	int a, b, c, A, B, C;
+	while (1) {
+		scanf("%d %d %d", &a, &b, &c);
+		if (a == 0 && b == 0 && c == 0) break;
+		A = a * a;
+		B = b * b;
+		C = c * c;
+		if (A > B && A > C) {
+			if (A == B + C) printf("right\n");
+			else printf("wrong\n");
+		}
+		else if (B > A && B > C) {
+			if (B == A + C) printf("right\n");
+			else printf("wrong\n");
+		}
+		else if (C > A && C > B) {
+			if (C == B + A) printf("right\n");
+			else printf("wrong\n");
+		}
+	}
 	return 0;
 }
