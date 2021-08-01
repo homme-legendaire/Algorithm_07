0bd703ea54012cb25280b1f05fa57ecc7a25cdde
homme-legendaire <53377700+homme-legendaire@users.noreply.github.com>
2021-08-01 20:47:50 +0900

    제로

---
+++ 10773.c
@@ -0,0 +1,51 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct NODE {
+	struct NODE* next;
+	int data;
+}NODE;
+void getNode(NODE** p) {
+	(*p) = (NODE*)malloc(sizeof(NODE));
+	(*p)->next = NULL;
+}
+void push(NODE* p, int e) {
+	NODE* q = NULL;
+	getNode(&q);
+	q->data = e;
+	q->next = p->next;
+	p->next = q;
+}
+void pop(NODE* p) {
+	NODE* q = NULL;
+	getNode(&q);
+	q = p;
+	p = p->next;
+	q->next = (q->next)->next;
+}
+int S(NODE* p) {
+	NODE* q = p->next;
+	int sum = 0;
+	while (q != NULL) {
+		sum += q->data;
+		q = q->next;
+	}
+	return sum;
+}
+int main() {
+	int k, e;
+	NODE* p = NULL;
+	getNode(&p);
+	scanf("%d", &k);
+	for (int i = 0; i < k; i++) {
+		scanf("%d", &e);
+		if (e != 0) {
+			push(p, e);
+		}
+		else {
+			pop(p);
+		}
+	}
+	printf("%d\n", S(p));
+}
