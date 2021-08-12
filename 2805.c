fd7709936eca93a36914edacf52ac353cbc13127
homme-legendaire <53377700+homme-legendaire@users.noreply.github.com>
2021-08-12 17:20:17 +0900

    나무 자르다가 손목 잘리겠어

--- 2805.c
+++ 2805.c
@@ -2,42 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void heap(int* heap, int num) {
-	for (int i = 1; i < num; i++) {
-		int c = i;
-		while (c != 0) {
-			int root = (c - 1) / 2;
-			if (heap[root] < heap[c]) {
-				int tmp = heap[root];
-				heap[root] = heap[c];
-				heap[c] = tmp;
-			}
-			c = root;
-		}
-	}
-	for (int i = num - 1; i >= 0; i--) {
-		int tmp = heap[0];
-		heap[0] = heap[i];
-		heap[i] = tmp;
-		int root = 0;
-		int c = 1;
-		while (c < i) {
-			c = 2 * root + 1;
-			if (heap[c] < heap[c + 1] && c < i - 1) {
-				c++;
-			}
-			if (heap[root] < heap[c] && c < i) {
-				int tmp = heap[root];
-				heap[root] = heap[c];
-				heap[c] = tmp;
-			}
-			root = c;
-		}
-	}
-}
-int Wood(int* H, int N, int M, int start, int end) {
+long long H[1000000];
+long long WOOD(long long* H, long long N, long long M, long long start, long long end) {
 	while (start <= end) {
-		int sum = 0, middle = (start + end) / 2;
+		long long sum = 0, middle = (start + end) / 2;
 		for (int i = 0; i < N; i++) {
 			if (H[i] > middle) {
 				sum += H[i] - middle;
@@ -53,12 +21,13 @@ int Wood(int* H, int N, int M, int start, int end) {
 	return end;
 }
 int main() {
-	int N, M, * H = NULL;
-	scanf("%d %d", &N, &M);
-	H = (int*)malloc(N * sizeof(int));
-	for (int i = 0; i < N; i++) {
-		scanf("%d", &H[i]);
+	long long N, M, max = 0;
+	scanf("%lld %lld", &N, &M);
+	for (int i = 0; i < N; ++i) {
+		scanf("%lld", &H[i]);
+		if (max < H[i]) {
+			max = H[i];
+		}
 	}
-	heap(H, N);
-	printf("%d", Wood(H, N, M, H[0], H[N - 1]));
+	printf("%lld", WOOD(H, N, M, 0, max));
 }
