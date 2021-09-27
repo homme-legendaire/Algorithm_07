846a271a277800c68fe4b6a08398c6e1d0c46656
LEE <53377700+homme-legendaire@users.noreply.github.com>
2021-09-27 15:49:40 +0900

    2xn 타일링 2

---
+++ 11727.c
@@ -0,0 +1,16 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int dp[1001];
+int DP(int x) {
+	if (x == 0) return 1;
+	if (x == 1) return 3;
+	if (dp[x] != 0) return dp[x];
+	return dp[x] = (DP(x - 1) + 2 * DP(x - 2)) % 10007;
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%d", DP(n - 1));
+}
